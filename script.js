b172d3ffacb22010e65ac7ec3a70d94b0b73c228
Michael Mercer <michaelmercer89@gmail.com>
2022-09-17 17:40:43 -0400

    basic layout, pre-js functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const screen = document.getElementById("screen");
+const numberButtons = document.querySelectorAll(".numbers")
+
+
+//screen.textContent = "fart";
+console.log({numberButtons});
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+     return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
